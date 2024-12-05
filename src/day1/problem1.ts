387725bae58e101c05a3b8d9f7d3b944905a45ed
Andrew Ong <andong@sas.upenn.edu>
2024-12-04 19:20:57 -0500

    Feat: Problem 1

--- src/day1/problem1.ts
+++ src/day1/problem1.ts
@@ -3,7 +3,28 @@ import readText from "../utils/readText";
 const problem1 = () => {
   const text = readText("./problem1.txt");
 
-  return text.split(" ").slice(0, 10);
+  const list1: number[] = [];
+  const list2: number[] = [];
+
+  const lines = text.split("\n");
+  lines.forEach((line) => {
+    const [num1, num2] = line.split(" ").filter(Boolean);
+    list1.push(parseInt(num1));
+    list2.push(parseInt(num2));
+  });
+
+  list1.sort();
+  list2.sort();
+
+  let runningDifference = 0;
+
+  for (let i = 0; i < list1.length; i++) {
+    const difference = Math.abs(list1[i] - list2[i]);
+
+    runningDifference += difference;
+  }
+
+  return runningDifference;
 };
 
 const answer = problem1();
